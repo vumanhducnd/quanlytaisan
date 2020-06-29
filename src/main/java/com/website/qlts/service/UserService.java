666ed15e47c18bf45daffbd8aa0dbcb1d203a5c5
buituananh2098 <hoinv@namviet-corp.com.vn>
2020-06-29 10:11:37 +0700

    add userService

---
+++ src/main/java/com/website/qlts/service/UserService.java
@@ -0,0 +1,5 @@
+package com.website.qlts.service;
+
+public interface UserService  {
+
+}
---
+++ src/main/java/com/website/qlts/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.website.qlts.service.impl;
+
+import com.website.qlts.models.User;
+import com.website.qlts.repository.UserRepository;
+import com.website.qlts.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        User tmp = userRepository.save(user);
+        return tmp;
+    }
+}
