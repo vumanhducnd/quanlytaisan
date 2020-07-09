05232d9dea03ce64d7c7206486fc525dea3aeef6
buituananh2098 <50397956+buituananh2098@users.noreply.github.com>
2020-07-09 09:45:17 +0700

    update department

--- src/main/java/com/website/qlts/service/impl/DepartmentsServiceImpl.java
+++ src/main/java/com/website/qlts/service/impl/DepartmentsServiceImpl.java
@@ -12,18 +12,38 @@ import java.util.List;
 public class DepartmentsServiceImpl implements DepartmentsService {
     @Autowired
     DepartmentsRepository departmentsRepository;
+
+    @Override
+    public Departments create(String name) {
+        Departments departments = new Departments();
+        departments.setDepartmentName(name);
+        departmentsRepository.save(departments);
+        return null;
+    }
+
     @Override
     public List<Departments> getAll() {
         return departmentsRepository.getAll();
     }
 
     @Override
-    public List<Departments> getByParentId(int id) {
+    public Departments getById(long id) {
         return null;
     }
 
     @Override
-    public List<Departments> getByChildId(int id) {
+    public void update(long id, String name) {
+
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public List<Departments> getByName(String name) {
         return null;
     }
 }
+
